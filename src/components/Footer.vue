f96f49bb66afff89e8284ee11cbf866d47fc4493
Mavlon4658 <mavlon4658@gmail.com>
2023-07-18 06:48:18 +0500

    glavni sahifa deyarli tayyor

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <div class="main_container">
+      <router-link class="link" to="/">
+        <img src="@/assets/images/footer_logo.png" alt="">
+      </router-link>
+
+      <div class="text">Copyright© 2023, Сервис электронного обмена валюты</div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="main_container">
+      <div class="header_wrapper">
+        <router-link class="logo" to="/">
+          <img src="@/assets/images/logo.svg" alt="logo">
+        </router-link>
+
+        <div class="header_menu">
+          <router-link class="menu_link" to="/">Обменник</router-link>
+          <router-link class="menu_link" to="/">Правила обмена</router-link>
+          <router-link class="menu_link" to="/">FAQ</router-link>
+          <router-link class="menu_link" to="/">Контакты</router-link>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
