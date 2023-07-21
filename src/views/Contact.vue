fc3521671a40af777764647500797746a4166573
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-07-21 23:40:37 +0500

    Contact qismi tayyor

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,8 +1,52 @@
 <template>
   <div class="contact">
     <div class="main_container">
-        <div class="content">this is contact</div>
+      <div class="contact_title">
+        <h2>Контакты</h2>
+      </div>
+      <div class="contact_main">
+        <div class="contact_main_left">
+          <div class="contact_block">
+            <div class="contact_block_icon">
+              <img src="@/assets/images/contact_icon_Time.svg" alt="">
+            </div>
+            <div class="contact_block_text">
+              <span>Работаем круглосуточно</span>
+            </div>
+          </div>
+          <div class="contact_block">
+            <div class="contact_block_icon">
+              <img src="@/assets/images/contact_icon_send.svg" alt="">
+            </div>
+            <div class="contact_block_text">
+              <span>Мы в Telegram:</span>
+              <a href="#">Напишите нам</a>
+            </div>
+          </div>
+          <div class="contact_block">
+            <div class="contact_block_icon">
+              <img src="@/assets/images/contact_icon_Message.svg" alt="">
+            </div>
+            <div class="contact_block_text">
+              <span>Поддержка по почте:</span>
+              <a href="#">CryptoExchanger@gmail.com</a>
+            </div>
+          </div>
+        </div>
+        <div class="contact_main_right">
+          <div class="contact_block">
+            <div class="contact_block_icon">
+              <img src="@/assets/images/contact_icon_Chat.svg" alt="">
+            </div>
+            <div class="contact_block_text">
+              <p>По любым вопросам вы можете обратиться в чат. Оператор свяжется с вами в течение 10 минут и постарается помочь</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    <img class="contact_bg_bottom" src="@/assets/images/contact_bg_bottom.png" alt="">
+    <!-- <img class="contact_bg_top" src="@/assets/images/contact_bg_top.png" alt=""> -->
   </div>
 </template>
 
