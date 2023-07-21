2797a5b07d0963febe8ef958991f5b3415286965
Mavlon4658 <mavlon4658@gmail.com>
2023-07-21 06:50:10 +0500

    Ikki kishilik ishlashga tayyor

---
+++ src/views/Faq.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="faq">
+    <div class="main_container">
+        <div class="content">this is faq</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Rules.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="rules">
+    <div class="main_container">
+      <div class="content">
+        this is rules
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
